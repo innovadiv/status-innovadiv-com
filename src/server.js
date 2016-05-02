e107847e91ed7ac450c11a0a37b71689b1a9dc87
Matt Lo <mattlo.developer@gmail.com>
2016-05-02 16:00:34 -0500

    added express, swig, and babel

--- src/server.js
+++ src/server.js
@@ -1,8 +1,26 @@
-const http = require('http');
+const app = require('express')();
+const swig = require('swig');
 
-http.createServer( (request, response) => {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
-}).listen(8124);
+const env = process.env.NODE_ENV || '';
+const envIsProd = env === 'production';
 
-console.log('Server running at http://127.0.0.1:8124/');
+// This is where all the magic happens!
+app.engine('swig', swig.renderFile);
+
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+// Swig will cache templates for you, but you can disable
+// that and use Express's caching instead, if you like:
+app.set('view cache', envIsProd);
+// To disable Swig's cache, do the following:
+swig.setDefaults({cache: envIsProd});
+// NOTE: You should always cache templates in a production environment.
+// Don't leave both of these to `false` in production!
+
+app.get('/', function (req, res) {
+  res.render('index.swig', { /* template locals context */ });
+});
+
+app.listen(8124);
+console.log('Application launched on http://localhost:8124');
